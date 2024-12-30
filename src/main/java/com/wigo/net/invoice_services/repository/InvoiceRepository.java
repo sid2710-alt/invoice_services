5266f24cea7d28870763bac16189e46bd34a95d2
Siddhant Chauhan <siddhantchauhan@Siddhants-MacBook-Air.local>
2024-12-31 00:37:31 +0530

    fixing serveice class

--- src/main/java/com/wigo/net/invoice_services/repository/InvoiceRepository.java
+++ src/main/java/com/wigo/net/invoice_services/repository/InvoiceRepository.java
@@ -1,7 +1,12 @@
 package com.wigo.net.invoice_services.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.wigo.net.invoice_services.model.Invoice;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+    public void deleteById(String id);
+
+    public boolean existsById(String id);
 }
